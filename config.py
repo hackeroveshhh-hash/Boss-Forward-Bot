7dfa45820e1c12e873250ebc3e2ab8607a7b003d
hackeroveshhh-hash <hackeroveshhh@gmail.com>
2026-03-02 09:08:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,3 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
 from os import environ 
 
 class Config:
@@ -12,10 +8,9 @@ class Config:
     DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://bosstgbots_db_user:DiRFdWd2U9kHoP4j@cluster0.g6p3m4j.mongodb.net/?appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
     BOT_OWNER = int(environ.get("BOT_OWNER", "1416433622"))
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+    # New Configs
+    LOG_ID = int(environ.get("LOG_ID", "-1003166629808"))
+    WELCOME_IMG = "https://graph.org/file/f340b55f492b0ad0276a9-24b7dabf4b19a8d723.jpg"
 
 class temp(object): 
     lock = {}
@@ -23,11 +18,3 @@ class temp(object):
     forwardings = 0
     BANNED_USERS = []
     IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-
-
-
